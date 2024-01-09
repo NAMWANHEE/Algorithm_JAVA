4d44be1ce361818ff2c53fbffa41b6af8d4e428f
NAMWANHEE <xjffla12@naver.com>
2024-01-09 21:20:34 +0900

    bj_1374

--- Baekjoon_algorithm/src/bj_1374.java
+++ Baekjoon_algorithm/src/bj_1374.java
@@ -12,7 +12,7 @@ public class bj_1374 {
 		StringTokenizer st;
 		int n = Integer.parseInt(br.readLine());
 		PriorityQueue<int[]> pq = new PriorityQueue<>((o1,o2)-> o1[0]-o2[0]);
-		PriorityQueue<Integer> pq2 = new PriorityQueue<>();
+		PriorityQueue<Integer> pq2 = new PriorityQueue<>(); // 강의실 별로 강의가 끝나는 시간 저장
 		for(int i=0;i<n;i++) {
 			st = new StringTokenizer(br.readLine());
 			int cla = Integer.parseInt(st.nextToken());
@@ -20,19 +20,15 @@ public class bj_1374 {
 			int e = Integer.parseInt(st.nextToken());
 			pq.add(new int[] {s,e});
 		}
-		int ans = 0;
 		while(!pq.isEmpty()) {
 			int [] arr = pq.poll();
 			System.out.println(Arrays.toString(arr));
-			while(!pq2.isEmpty() && pq2.peek() <= arr[0]) {
+			while(!pq2.isEmpty() && pq2.peek() <= arr[0]) { // 가장 강의가 빨리 끝나는 시간보다 강의가 늦게 시작하는 경우 강의실의 끝나는 시간만 변경 아닌 경우에는 강의실 추가
 				pq2.poll();
 				break;
 			}
-			pq2.add(arr[1]);
-			System.out.println(pq2);
-			ans = Math.max(ans, pq2.size());
+			pq2.add(arr[1]);	// 강의실 추가
 		}
-		System.out.println(ans);
 		System.out.println(pq2.size());
 
 	}
